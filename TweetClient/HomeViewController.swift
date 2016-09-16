81454c7eefdb9a8c09d36b69c287a65a53b1e2c8
Vincent Chau <vichau@ucdavis.edu>
2016-09-16 15:06:23 -0700

    update on parser

--- TweetClient/HomeViewController.swift
+++ TweetClient/HomeViewController.swift
@@ -11,25 +11,36 @@ import UIKit
 class HomeViewController: UIViewController
 {
 
+    var allTweets: [Tweet]? {
+        didSet {
+            // Reload table view data
+            print("did set tweets")
+        }
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        
-        
+        updateWithTestJSON()
         
     }
+    
     override func viewWillAppear(_ animated: Bool)
     {
         super.viewWillAppear(animated)
-    
     }
+    
     override func didReceiveMemoryWarning()
     {
-    
         super.didReceiveMemoryWarning()
     }
     
+    func updateWithTestJSON()
+    {
+        let testData = JSONParserUtil.loadTestJSON()
+        JSONParserUtil.parseJSON(data: testData!)
+    }
     
     
 }
--- TweetClient/JSONParserUtil.swift
+++ TweetClient/JSONParserUtil.swift
@@ -10,9 +10,44 @@ import UIKit
 
 class JSONParserUtil
 {
+    /*  Parser Error Types */
+    enum ParserError: Error {
+        case invalidUID
+        case invalidText
+    }
     
-    
+    /*  Parse JSON from an NSData object */
+    class func parseJSON(data: NSData) -> [Tweet]?
+    {
+        var tweets: [Tweet] = []
+        do {
 
+            let jsonData = try JSONSerialization.jsonObject(with: data as Data, options: []) as? [[String: AnyObject]]
+            
+            for tweetDict in jsonData! {
+                
+                guard let uid = tweetDict["user"]?["name"] as? String
+                    else {  throw ParserError.invalidUID }
+
+                guard let text = tweetDict["text"] as? String
+                    else {  throw ParserError.invalidText }
+                
+                print(uid)
+                print(text)
+            
+                let tweet = Tweet(text: text, uid: uid)
+                tweets.append(tweet)
+            }
+            
+            return tweets;
+
+        } catch {
+            print("error serializing JSON: \(error)")
+        }
+        
+        
+        return nil
+    }
 
     /*  Load Test JSON Data to parse */
     class func loadTestJSON() -> NSData?
@@ -20,7 +55,6 @@ class JSONParserUtil
         var jsonData: NSData?
         if let path = Bundle.main.path(forResource: "testing", ofType: "json")
         {
-            
             do {
                 let data = try NSData(contentsOfFile: path, options: .mappedIfSafe)
                 jsonData = data
